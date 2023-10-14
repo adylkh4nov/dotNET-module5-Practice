97c7a64be9b3e64715128594117ddbf24a5886d1
Olzhas Adylkhanov <adylkh4nov@gmail.com>
2023-10-14 23:28:09 +0600

    Добавьте файлы проекта.

---
+++ dotNET-module5-Practice/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Module5Practice
+{
+    public class Module5Practice
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CallMethod();
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine($"Custom Exception: {ex.Message}");
+            }
+        }
+
+        static void CallMethod()
+        {
+            try
+            {
+                AnotherMethod();
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine("Exception handled in CallMethod.");
+                throw;
+            }
+        }
+
+        static void AnotherMethod()
+        {
+            throw new CustomException("Custom Exception thrown from AnotherMethod.");
+        }
+    }
+
+    class CustomException : Exception
+    {
+        public CustomException(string message) : base(message) { }
+    }
+}
